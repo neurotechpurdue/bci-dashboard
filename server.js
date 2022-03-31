a69b2afd08398fe921c34590d37cb4b5c0a32ac2
victoirebeaufils <victoire.beaufil@gmail.com>
2022-03-30 20:15:01 -0400

    C:\Users\lavie\Documents\projects\web\bci-dashboard\nsp-2022-171b2a6451a8.json

--- server.js
+++ server.js
@@ -1,31 +1,82 @@
-//environment variables
+const {Storage} = require('@google-cloud/storage');
+
+
+
 var dotenv = require('dotenv').config()
 const osc = require('node-osc');
-// general imports
 const express = require('express')
-const { Server } = require('http')
-
-//create an express app
 const app = express()
-const port = process.env.PORT || 8000 
-app.listen(port, ()=> console.log(`server running on port ${port}`))
+const http = require('http');
+const server = http.createServer(app);
+const socketio = require("socket.io");
+var cors = require('cors');
+
+app.use(cors());
+var io = socketio(server,{
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+  });
+
+  io.on('connection', (socket) => {
+    socket.emit('message', "HELLO CAN ANYONE HEAR ME")
+  })
+
+const port = 8000 
+
 const OSC_PORT = 12345;
 const oscServer = new osc.Server(OSC_PORT, '127.0.0.1');
 oscServer.on('message', data => handleMessage(data)); //
+let interval;
+
+const storage = new Storage({
+  projectId: process.env.PROJECT_ID,
+  credentials: {
+    client_email: process.env.CLIENT_EMAIL,
+    private_key: process.env.PRIVATE_KEY
+  }
+});
+
+console.log("storage:" + storage);
+
+async function listBuckets() {
+  try {
+    const results = await storage.getBuckets();
+
+    const [buckets] = results;
+
+    console.log('Buckets:');
+    buckets.forEach(bucket => {
+      console.log(bucket.name);
+    });
+  } catch (err) {
+    console.error('ERROR:', err);
+  }
+}
+listBuckets();
 
 
 const handleMessage = (data) =>{
     console.log(data);
-    // if(data[0] == 'fft' ) {
-    //     handleFFTData(data)
+     if(data[0] == 'fft' ) {
+         console.log("handling fft data")
+         handleFFTData(data)
     // }
     // else {
     // handleTimeSeries(data);
     // }
 }
+}
 
-// app.get('/osc', (req, res)=> {
+const handleFFTData = (data) => {
 
-// })
+    if(data[1] == 1) { //channel 1
+
+    }
+
+}
 
 
+
+  server.listen(app.get(port), ()=> console.log(`server running on port ${port}`))
